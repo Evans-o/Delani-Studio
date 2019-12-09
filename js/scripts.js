1ebb58d2fcc7d2c66480c002f6869a9732f222d7
Evans-o <evansopindi@yahoo.com>
2019-12-09 15:16:26 +0300

    added initial structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     $("form#form").submit(function(event){
+      // event.preventDefault();
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
-      var message = $("input#comment").val();
+      var message = $("textarea#comment").val();
       if ($("input#MERGE1").val() && $("input#MERGE0").val()){
         alert (name + ", we have received your message. Thank you for reaching out to us.");
       }
       else {
         alert("Please enter your name and email!");
       }
-      event.reventDefault()
       
     });
   
@@ -46,8 +46,4 @@ $(document).ready(function(){
       $("#product-image").slideDown('1500');
     });
   });
-  $(".#work1").hover(function(){
-    $('#work2').toggle();
-  })
-  
   
