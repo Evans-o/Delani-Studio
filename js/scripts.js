f6c191a491580014737753332e31f9e4f272c3de
Evans-o <evansopindi@yahoo.com>
2019-12-12 23:49:48 +0300

    modified body

--- js/scripts.js
+++ js/scripts.js
@@ -31,25 +31,21 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $("#work1").mouseover(function(){
-    $("#overlay").show();
-  }).mouseout(function(){
-    $("#overlay").hide();
-  });
+$(document).ready(function() {
+  $("#work1").hover(
+    function(){
+      $("#overlay").show("200");
+    },
+    function() {
+      $("#overlay").hide(3000);
+    }
+  );
 });
 $(document).ready(function(){
-  $("#work2").mouseover(function(){
-    $("#overlay2").show();
-  }).mouseout(function(){
-    $("#overlay2").hide();
-  });
-});
-$(document).ready(function(){
-  $("#work3").mouseover(function(){
-    $("#overlay3").show();
-  }).mouseout(function(){
-    $("#overlay3").hide();
+  $("#work3").hover(function(){
+    $("#overlay3").show("200");
+  },function(){
+    $("#overlay3").hide(3000);
   });
 });
 $(document).ready(function(){
