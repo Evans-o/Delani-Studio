9cc40e0d21c22b9570a1720f6e6552df5cd84bde
Evans-o <evansopindi@yahoo.com>
2019-12-12 22:11:17 +0300

    modified body

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,7 @@
 $(document).ready(function(){
-  $("form#form").submit(function(event){ 
-    var name = $("input#MERGE1").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-     alert ( "Hi   " +  name + ", we have received your message. Thank you for reaching out to us.");
-      $("form#form").allowDefault = true;
-    }
-    else if($("textarea#comment"===null)) {
-      alert("please enter your name,email and your message!");
-      event.preventDefault()
-    }
-    else if($("input#MERGE0"===null)) {
-      alert("please enter your email");
-      event.preventDefault()
-    }
-    else {
-      alert("Please enter your name and email!");
-      event.preventDefault()
-    }
-  });
-});
-$(document).ready(function(){
   $("#development-image").click(function(){
-    $("#development-image").slideDown(3000).hide(3000);
-    $("#development").show(5000);
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
   });
   $("#development").click(function(){
     $("#development").slideUp('1500');
@@ -53,72 +30,72 @@ $(document).ready(function(){
     $("#product-image").slideDown('1500');
   });
 });
-$(document).ready(function() {
-  $("#work1").hover(
-    function() {
-      $("#overlay").show("200");
-    },
-    function() {
-      $("#overlay").hide(3000);
-    }
-  )
-  $("#work2").hover(
-    function() {
-      $("#overlay2").show("200");
-    },
-    function() {
-      $("#overlay2").hide(3000);
-    }
-  )
-  $("#work3").hover(
-    function() {
-      $("#overlay3").show("200");
-    },
-    function() {
-      $("#overlay3").hide(3000);
-    }
-  )
-  $("#work4").hover(
-    function() {
-      $("#overlay4").show("200");
-    },
-    function() {
-      $("#overlay4").hide(3000);
-    }
-  )
-  $("#work5").hover(
-    function() {
-      $("#overlay5").show("200");
-    },
-    function() {
-      $("#overlay5").hide(3000);
-    }
-  )
-  $("#work6").hover(
-    function() {
-      $("#overlay6").show("200");
-    },
-    function() {
-      $("#overlay6").hide(3000);
-    }
-  )
-  $("#work7").hover(
-    function() {
-     $("#overlay7").show("200");
-    },
-    function() {
-      $("#overlay7").hide(3000);
-    }
-  )
-  $("#work8").hover(
-    function() {
-      $("#overlay8").show("200");
-    },
-    function() {
-      $("#overlay8").hide(3000);
-    }
-  )
-})
 
+$(document).ready(function(){
+  $("#work1").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
 
-  
+$(document).ready(function(){
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("form#form34A").submit(function(event){
+    // event.preventDefault();
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+    
+  });
+
+});
